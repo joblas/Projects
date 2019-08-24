fbfb260e0a03cf9eb021e532fc55a1d1dd4d7d9b
joblas <josephablas@gmail.com>
2019-08-24 11:22:28 -0700

    put guessinggame code back in file

---
+++ guessinggame.sh
@@ -0,0 +1,17 @@
+guess=-1
+typeset -i num=0
+answer=$(ls -A| wc -l)
+
+echo "Guess how may files in current directory"
+
+# Guess how many files
+while (( guess != answer )); do
+	num=num+1
+	read -p "Enter guess $num: " guess
+	if (( guess < answer )); then
+		echo "Higher..."
+	elif (( guess > answer )); then
+		echo "Lower..."
+	fi
+done
+echo -e "Correct! That took $num guesses.\n"
