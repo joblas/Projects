5132c3a394ef4d2b25c020925bdb191950aea690
joblas <josephablas@gmail.com>
2019-08-31 16:28:33 -0700

    added spaces correct grammer

--- guessinggame.sh
+++ guessinggame.sh
@@ -2,23 +2,26 @@
 # File: guessinggame.sh
 # 08-22-2019 Joseph BLas Created this.
 
+typeset -i num=0
+file_counter=$(ls -l | wc -l)
+
 function answer {
 	echo "Guess how many files in current directory"
 	read guess
 }
-typeset -i num=0
-file_counter=$(ls -l | wc -l)
-
 while [[ $guess != $file_counter ]]; do
 	num=num+1
 		if [[ -z $guess ]]; then
 				answer
 		elif [[ $guess < $file_counter ]]; then
+				echo ""
 				echo "Guess Higher..."
 				answer
 		elif [[ $guess > $file_counter ]]; then
+				echo ""
 				echo "Guess Lower..."
 				answer
 		fi
 done
 echo "Correct! You guessed $num times"
+echo ""
